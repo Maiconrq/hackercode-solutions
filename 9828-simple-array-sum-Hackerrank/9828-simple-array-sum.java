576d7101ec2de0660482f78a93025778c91d5bc1
Maicon Rodrigues Queiroz <nociamrq27@gmail.com>
2026-06-09 01:20:03 -0300

    Added solution - LeetHub

--- 9828-simple-array-sum-Hackerrank/9828-simple-array-sum.java
+++ 9828-simple-array-sum-Hackerrank/9828-simple-array-sum.java
@@ -50,3 +50,8 @@ public class Solution {
         bufferedWriter.close();
     }
 }
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
