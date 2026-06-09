2d48315d1bb2ab8ce0c21ea506e2f4ca12e21af7
Maicon Rodrigues Queiroz <nociamrq27@gmail.com>
2026-06-09 01:38:42 -0300

    Time: 47 ms (9.59%), Space: 47 MB (72.01%) - LeetHub - Time: O(n^2, Space: O(?)

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int current = 0;
-        int[] result;
+        //int[] result = new int[] {0, 0};
         for(int i = 0; i < nums.length; i++){
             for(int j = i + 1; j < nums.length; j++){
                 if(nums[i] + nums[j] == target){
@@ -12,3 +11,7 @@ class Solution {
         return new int[]{};
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
